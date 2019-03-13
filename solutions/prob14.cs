756596523c4f66d81a71de52422931606598f5c7
Ryan Broman <ryanjbroman@gmail.com>
2019-03-13 14:58:23 -0700

    prob14

--- solutions/prob14.cs
+++ solutions/prob14.cs
@@ -16,23 +16,25 @@ namespace euler {
         public void Problem14() {
             int largestChainLength = 0;
             int largestChainNumber = 0;
-            List<int> chain = null;
+            List<long> largestChain = null;
             for(int i = 0; i < 1000000; i++) {
+                List<long> chain;
                 chain = GenerateChain(i);
                 int length = chain.Count;
                 if(length > largestChainLength) {
                     largestChainLength = length;
                     largestChainNumber = i;
+                    largestChain = chain;
                 }
             }
-            foreach(int j in chain) {
+            foreach(int j in largestChain) {
                 Console.Write($"{j} -> ");
             }
-            Console.WriteLine($"\n\nThe value {largestChainNumber} had a {largestChainLength} long chain."); // :)
+            Console.WriteLine($"\n\nThe value {largestChainNumber} had a {largestChainLength} long chain.");
         }
-        List<int> GenerateChain(int n) {
+        static List<long> GenerateChain(long n) {
             // Generates a Collatz Problem chain given the starting value n.
-            var chain = new List<int>{};
+            var chain = new List<long>{n};
             while(n > 1) {
                 bool even = n % 2 == 0;
                 n = even ? n/2 : 3*n+1;
